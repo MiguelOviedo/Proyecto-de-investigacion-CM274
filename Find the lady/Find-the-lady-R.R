0d1fb9a622c1bae85be2f55182e396819e823bc1
DanielHCh <31776668+DanielHCh@users.noreply.github.com>
2018-06-24 17:43:47 -0500

    Corrección de ortografía y mejora de comentarios y código, y jupyter notebook del codigo agregado
    En el código:
    - Se agregó tablas plot para comparar algoritmos y se corregió errores ortográficos.
    - Se agregó el cuadernillo de jupyter del código entero.

--- Find the lady/Find-the-lady-R.R
+++ Find the lady/Find-the-lady-R.R
@@ -14,7 +14,8 @@ my.money <- 0
 my.card <- sample(my.card)
 
 #Escogemos una carta
-my.pick <- readline(prompt = "Elige una carta, de izquierda a derecha: ¿1, 2 o 3?: ")
+#my.pick <- readline(prompt = "Elige una carta, de izquierda a derecha: ¿1, 2 o 3?: ")
+my.pick <- sample(1:3,1)
 while(as.integer(my.pick) > 3 || as.integer(my.pick) < 1){
   my.pick <- readline(prompt = "Solo puedes elegir entre el 1 y el 3: ")
 }
@@ -26,7 +27,9 @@ if(my.card[as.integer(my.pick)] == "Queen of hearts"){
   my.money <- my.money - 1
 }
 
-#Ahora hagemos lo mismo pero en el caso de que se juegue hasta que ganemos por primera vez
+print(paste("Dinero obtenido: ", my.money))
+
+#Ahora hacemos lo mismo pero en el caso de que se juegue hasta que ganemos por primera vez (Las vegas algorithm - Algoritmo de Las Vegas: Nos asegura un resultado satisfactorio)
 #Cantidad de veces jugadas
 my.plays <- 0
 my.money <- 0
@@ -48,50 +51,137 @@ while(win != TRUE){
 print(paste("Dinero obtenido: ", my.money, " Veces jugadas:", my.plays))
 
 
-#Modulamos nuestro código para jugar sin importar si ganamos o perdemos, y veamos el resultado
+#Modulamos nuestro código para jugar una cantidad veces como máximo así no ganemos, y veamos el resultado (Monte Carlo algorithm - Algoritmo de Monte Carlo: En un tiempo finito buscamos la respuesta que deseamos)
 my.plays <- 0
 my.money <- 0
+win <- FALSE
 
-#Digamos que jugamos 1000 veces
-while(my.plays < 1001){
+#Digamos que jugamos 5 veces como máximo hasta ganar 1 vez
+while(my.plays < 5 && win == FALSE){
   my.card <- sample(my.card)
   my.pick <- sample(1:3,1)
   
   if(my.card[as.integer(my.pick)] == "Queen of hearts"){
     my.money <- my.money + 2
     my.plays <- my.plays +1
+    win <- TRUE
   } else{
     my.money <- my.money - 1
     my.plays <- my.plays +1
   }
 }
 
-my.expected[4] <- my.money
 print(paste("Dinero obtenido: ", my.money, " Veces jugadas:", my.plays))
 
-#Ahora tomaremos muestras de mil (1000) jugadas y las evaluaremos dentro de un array, la media y la desviación estandar
-my.expected <- c(1:1000)
-numExpect <- 0
+
+#En un caso especial, digamos que queremos obtener 50 victorias.
+#Veamos cuántas jugadas podría tomar con el algoritmo de Las Vegas
 my.plays <- 0
 my.money <- 0
-while(numExpect < 1001){
-  while(my.plays < 1001){
+my.wins <- 0
+
+while(my.wins < 500){
+  my.card <- sample(my.card)
+  my.pick <- sample(1:3,1)
+  
+  if(my.card[as.integer(my.pick)] == "Queen of hearts"){
+    my.money <- my.money + 2
+    my.plays <- my.plays + 1
+    my.wins <- my.wins + 1
+  } else{
+    my.money <- my.money - 1
+    my.plays <- my.plays + 1
+  }
+}
+
+print(paste("Dinero obtenido: ", my.money, " Veces jugadas:", my.plays))
+
+
+#Veamos qué pasa si queremos repetir dicha hazaña: 100 veces 500 victorias
+my.plays <- 0
+my.money <- 0
+my.wins <- 0
+my.gamesplayed <- c(1:100)
+my.expectedMoney <- c(1:100)
+rounds <- 1
+while(rounds < 101){
+  while(my.wins < 500){
     my.card <- sample(my.card)
     my.pick <- sample(1:3,1)
     
     if(my.card[as.integer(my.pick)] == "Queen of hearts"){
       my.money <- my.money + 2
-      my.plays <- my.plays +1
+      my.plays <- my.plays + 1
+      my.wins <- my.wins + 1
     } else{
       my.money <- my.money - 1
-      my.plays <- my.plays +1
+      my.plays <- my.plays + 1
     }
   }
-  my.expected[numExpect] <- my.money
+  my.gamesplayed[rounds] <- my.plays
+  my.expectedMoney[rounds] <- my.money
+  my.money <- 0
   my.plays <- 0
-  numExpect <- numExpect + 1
+  my.wins <- 0
+  rounds <- rounds + 1
 }
 
-my.expected
-mean(my.expected)
-sd(my.expected)
+print("Jugadas:")
+my.gamesplayed
+print("Dinero:")
+my.expectedMoney
+print(paste("Media: ", mean(my.expectedMoney), " Desviación estandar: ", sd(my.expectedMoney)))
+x <- data.frame("Partidas" = my.gamesplayed, "Dinero" = my.expectedMoney)         
+with(x,plot(Partidas,Dinero))
+
+#Ahora usaremos Monte Carlo para tratar de conseguir: 100 veces 500 victorias con un máximo de 1500 jugadas por vez
+my.plays <- 0
+my.money <- 0
+my.wins <- 0
+my.gamesplayed <- c(1:100)
+my.expectedMoney <- c(1:100)
+my.victory <- c(1:100)
+rounds <- 1
+my.victories <- 0
+while(rounds < 101){
+  while(my.wins < 500 && my.plays < 1500){
+    my.card <- sample(my.card)
+    my.pick <- sample(1:3,1)
+    
+    if(my.card[as.integer(my.pick)] == "Queen of hearts"){
+      my.money <- my.money + 2
+      my.plays <- my.plays + 1
+      my.wins <- my.wins + 1
+    } else{
+      my.money <- my.money - 1
+      my.plays <- my.plays + 1
+    }
+  }
+  my.gamesplayed[rounds] <- my.plays
+  my.expectedMoney[rounds] <- my.money
+  
+  if(my.wins == 500){
+    my.victory[rounds] <- "Win"
+    my.victories <- my.victories + 1
+  }
+  else{
+    my.victory[rounds] <- "Lose"
+  }
+  
+  my.money <- 0
+  my.plays <- 0
+  my.wins <- 0
+  rounds <- rounds + 1
+}
+
+print("Jugadas:")
+my.gamesplayed
+print("Dinero:")
+my.expectedMoney
+print("Gano?:")
+my.victory
+print(paste("Ganó unas: ", my.victories, " veces"))
+print(paste("Media: ", mean(my.expectedMoney), " Desviación estandar: ", sd(my.expectedMoney)))
+
+x <- data.frame("Partidas" = my.gamesplayed, "Dinero" = my.expectedMoney)
+with(x,plot(Partidas,Dinero))
